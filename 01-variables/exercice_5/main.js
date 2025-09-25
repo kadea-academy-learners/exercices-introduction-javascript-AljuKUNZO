be6b9c16d94507a68a510ab78fdd328e403348e8
alju kan <aljukunzonduakulu@gmail.com>
2025-09-25 18:56:28 +0100

    modification de l'exercice 6 et test reussi

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,17 +1,13 @@
 // Exercice 5
 // Écrivez votre code ici
-// Déclaration de la température en Celsius
-// Fonction demandée par le test
 function celsiusToFahrenheit(celsius) {
     return (celsius * 9/5) + 32;
 }
 
-// Exemple avec une variable comme l’énoncé
 let temperatureCelsius = 25;
 let temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius);
 
-console.log(temperatureFahrenheit); // 77
+console.log(temperatureFahrenheit);
 
-// Export pour Jest
-module.exports = celsiusToFahrenheit;
 
+module.exports = { celsiusToFahrenheit };
--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,23 +1,19 @@
 // Exercice 6
 // Écrivez votre code ici
-// 1) age : nombre
-let age = 20 ; 
+// Exercice 6 : Déclaration de variables de différents types
 
-// 2) nom : chaîne de caractères
-let nom = "Alju"; 
+let age = 20;                  // 1. Valeur numérique
+let nom = "Alju";              // 2. Chaîne de caractères
+let estConnecte = true;        // 3. Booléen
+let valeurNulle = null;        // 4. null
+let inconnue;                  // 5. Déclarée sans valeur
 
-// 3) estConnecte : booléen
-let estConnecte = true; 
-
-// 4) valeurNulle : null
-let valeurNulle = null;
-
-// 5) variable inconnue sans valeur
-let inconnue; 
-
-// Affichage pour vérifier
+// Affichage pour vérification manuelle
 console.log(age);
 console.log(nom);
 console.log(estConnecte);
 console.log(valeurNulle);
 console.log(inconnue);
+
+// Exportation pour Jest (tests)
+module.exports = { age, nom, estConnecte, valeurNulle, inconnue };
