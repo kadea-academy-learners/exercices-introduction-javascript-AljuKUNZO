6c70b89a66728d1bcd084d51f7f546d3950aa289
alju kan <aljukunzonduakulu@gmail.com>
2025-09-25 19:06:43 +0100

    modification de l'exercice 7 et passage de test

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,23 +1,29 @@
 // Exercice 7
 // Écrivez votre code ici
 // 1) Age de l'enfant
-let ageEnfant = 10; 
+// Exercice 7 : Calcul des âges
 
-// 2) Age du père = double de l'âge de l'enfant
+// 1. Âge de l’enfant
+let ageEnfant = 10;
+
+// 2. Âge du père = double de l’âge de l’enfant
 let agePere = ageEnfant * 2;
 
-// 3) Age de la mère = âge du père moins 5
+// 3. Âge de la mère = âge du père - 5
 let ageMere = agePere - 5;
 
-// 4) Age du grand-père = double de l'âge de la mère plus moitié de l'âge de l'enfant
+// 4. Âge du grand-père = (2 × âge de la mère) + (½ × âge de l’enfant)
 let ageGrandPere = (ageMere * 2) + (ageEnfant / 2);
 
-// 5) Age de l'oncle = âge du père plus 10
+// 5. Âge de l’oncle = âge du père + 10
 let ageOncle = agePere + 10;
 
-// Affichage pour vérifier
-console.log("Enfant :", ageEnfant);
-console.log("Père :", agePere);
-console.log("Mère :", ageMere);
-console.log("Grand-père :", ageGrandPere);
-console.log("Oncle :", ageOncle);
+// Affichage console (optionnel pour vérifier)
+console.log("Enfant:", ageEnfant);
+console.log("Père:", agePere);
+console.log("Mère:", ageMere);
+console.log("Grand-père:", ageGrandPere);
+console.log("Oncle:", ageOncle);
+
+// Exportation pour Jest
+module.exports = { ageEnfant, agePere, ageMere, ageGrandPere, ageOncle };
