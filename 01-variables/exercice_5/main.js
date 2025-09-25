69861978c42cdcb319bd3496c3d4d6193dfeb396
alju kan <aljukunzonduakulu@gmail.com>
2025-09-25 18:36:27 +0100

    modification de main de l'exercice 5

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,10 +1,17 @@
 // Exercice 5
 // Écrivez votre code ici
 // Déclaration de la température en Celsius
-let celsius = 25; 
+// Fonction demandée par le test
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
 
-// Conversion en Fahrenheit
-let fahrenheit = (celsius * 9/5) + 32;
+// Exemple avec une variable comme l’énoncé
+let temperatureCelsius = 25;
+let temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius);
 
-// Affichage du résultat
-console.log(fahrenheit);
+console.log(temperatureFahrenheit); // 77
+
+// Export pour Jest
+module.exports = celsiusToFahrenheit;
+
